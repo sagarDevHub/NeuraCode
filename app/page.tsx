d4f27a5589cb8e64077396706ad7fbec3af98880
sagarDevHub <gullasagar9933@gmail.com>
2025-10-04 18:56:26 +0530

    01 Initial Setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <Button>Get Started</Button>
+    </div>
+  );
+};
+
+export default Home;
